1d6738dbb3f0ae24df60ec62688c9640de82899e
dizefurkan <dizefurkan@gmail.com>
2018-07-03 09:46:20 +0300

    app file was removed to src file. added to gitignore page -.vscode. and jwt secret key was changed.

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import expressCore from './core/express';
+import { server } from './config';
+
+const app = express();
+
+expressCore.forEach(item => app.use(item));
+
+app.listen(server.port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server listening at localhost:%s", server.port);
+    }
+});
